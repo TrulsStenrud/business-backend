c037f300dd478222eadeaabf775c45d19f28847a
Truls <truls.stenrud@gmail.com>
2023-01-16 18:29:32 +0100

    Init commit

--- src/main/kotlin/no/gutta/Application.kt
+++ src/main/kotlin/no/gutta/Application.kt
@@ -1,18 +1,20 @@
 package no.gutta
 
-import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import no.gutta.plugins.*
+import io.ktor.server.application.Application
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import no.gutta.plugins.configureHTTP
+import no.gutta.plugins.configureRouting
+import no.gutta.plugins.configureSerialization
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+  embeddedServer(Netty, port = 8080, host = "127.0.0.1", module = Application::module)
+    .start(wait = true)
 }
 
 fun Application.module() {
-    configureSecurity()
-    configureHTTP()
-    configureSerialization()
-    configureRouting()
+//  configureSecurity()
+  configureHTTP()
+  configureSerialization()
+  configureRouting()
 }
--- src/main/kotlin/no/gutta/plugins/HTTP.kt
+++ src/main/kotlin/no/gutta/plugins/HTTP.kt
@@ -1,15 +1,11 @@
 package no.gutta.plugins
 
-import io.ktor.server.plugins.defaultheaders.*
-import io.ktor.server.plugins.openapi.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.defaultheaders.DefaultHeaders
 
 fun Application.configureHTTP() {
-    install(DefaultHeaders) {
-        header("X-Engine", "Ktor") // will send this header with each response
-    }
-    openAPI(path = "openapi")
-
+  install(DefaultHeaders) {
+    header("X-Engine", "Ktor") // will send this header with each response
+  }
 }
