87bf8056bd7e001dbd784c537289e77252f9a941
Truls <truls.stenrud@gmail.com>
2023-01-21 10:55:23 +0100

    Make the infra-code work

--- cdk/app-stack.ts
+++ cdk/app-stack.ts
@@ -1,8 +1,9 @@
 import * as cdk from "aws-cdk-lib";
+import {StackProps} from "aws-cdk-lib";
 import * as constructs from "constructs";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
-import { StackProps } from "aws-cdk-lib";
+import * as elb from "aws-cdk-lib/aws-elasticloadbalancingv2"
 
 
 interface AppStackProps extends StackProps {
@@ -14,43 +15,91 @@ export class AppStack extends cdk.Stack {
     super(scope, id, props);
 
     const vpc = new ec2.Vpc(this, "Vpc", {
-      natGateways: 1,
+
     });
 
     const cluster = new ecs.Cluster(this, "Cluster", {
       vpc: vpc,
     });
 
-    const taskDefinition = new ecs.TaskDefinition(this, "TaskDefinition", {
-      compatibility: ecs.Compatibility.FARGATE,
-      cpu:  "256",
-      memoryMiB:  "512",
-      networkMode: ecs.NetworkMode.AWS_VPC,
-    });
+    const taskDefinition = new ecs.FargateTaskDefinition(
+      this,
+      "TaskDefinition",
+      {
+        cpu: 256,
+        memoryLimitMiB: 512,
+      },
+    )
+
     taskDefinition.addContainer("Container", {
       image: ecs.ContainerImage.fromAsset("."),
+      logging: ecs.LogDrivers.awsLogs({streamPrefix: 'bussness-log-group', logRetention: 5}),
+      linuxParameters: new ecs.LinuxParameters(this, "LinuxParameters", {
+        initProcessEnabled: true,
+      }),
       portMappings: [
         {
           containerPort: 8080,
+          hostPort: 8080,
         },
       ],
     });
 
-
-
     const securityGroup = new ec2.SecurityGroup(this, "SecurityGroup", {
       vpc: vpc,
-      allowAllOutbound: true,
     });
 
-    const fargateService = new ecs.FargateService(this, "Task", {
+    const service = new ecs.FargateService(this, "Task", {
+      serviceName: "bussiness-backend",
       cluster: cluster,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
       taskDefinition: taskDefinition,
-      enableExecuteCommand: true,
       platformVersion: ecs.FargatePlatformVersion.VERSION1_4,
       desiredCount: 1,
+      enableExecuteCommand: true,
       securityGroups: [securityGroup],
+      assignPublicIp: true,
     });
+
+
+    const targetGroup = new elb.ApplicationTargetGroup(this, "TargetGroup", {
+      protocol: elb.ApplicationProtocol.HTTP,
+      port: 8080,
+      vpc: vpc,
+      targetType: elb.TargetType.IP,
+      targets: [service],
+      deregistrationDelay:
+        cdk.Duration.seconds(15),
+    })
+
+    const loadBalancer = new elb.ApplicationLoadBalancer(this, "LoadBalancer", {
+      vpc: vpc,
+      internetFacing: true,
+      vpcSubnets: vpc.selectSubnets({
+        subnetType: ec2.SubnetType.PUBLIC,
+      }),
+    })
+
+    const httpListener = loadBalancer.addListener("HttpListener", {
+      // sslPolicy: elb.SslPolicy.TLS12,
+      protocol: elb.ApplicationProtocol.HTTP,
+      port: 443,
+      certificates: undefined,
+      defaultTargetGroups: [targetGroup],
+    })
+
+    const applicationListenerRule = new elb.ApplicationListenerRule(
+      this,
+      "ListenerRule",
+      {
+        listener: httpListener,
+        priority: 1,
+        conditions: [elb.ListenerCondition.pathPatterns(["/*"])],
+        targetGroups: [targetGroup],
+      },
+    )
 
   }
 }
