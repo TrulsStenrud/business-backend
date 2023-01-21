5a0a96d0d3b0edc1ef5e159aadcd635e3541acb3
Truls <truls.stenrud@gmail.com>
2023-01-21 11:40:02 +0100

    Test mulitcommit

--- src/main/kotlin/no/gutta/Application.kt
+++ src/main/kotlin/no/gutta/Application.kt
@@ -11,7 +11,7 @@ fun main() {
   embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
     .start(wait = true)
 }
-
+// testComment
 fun Application.module() {
 //  configureSecurity()
   configureHTTP()
